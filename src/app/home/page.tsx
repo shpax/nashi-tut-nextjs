ff23634db40fc04e41129245d709b2aea0cd5fa2
Maxim Piven <shpax1@gmai.com>
2024-11-28 09:08:46 -0500

    scale

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,19 +13,19 @@ export default async function Page() {
       href={category.path}
       className={`block  relative ${
         category.locations > 0 ? 'bg-secondary' : 'bg-gray-500'
-      }  flex flex-row gap-2 items-center rounded-full w-40 h-16 shadow text-gray-100`}
+      }  flex flex-row gap-2 items-center rounded-full w-40 h-12 shadow text-gray-100`}
     >
       <div
-        className={`h-full aspect-square px-3 ${
+        className={`h-full aspect-square px-1 ${
           category.locations > 0 ? 'bg-secondary-200' : 'bg-gray-400'
         } rounded-full flex justify-center items-center`}
       >
-        <i className={`${category.icon} mt-1 text-3xl`} />
+        <i className={`${category.icon} mt-1 text-xl`} />
       </div>
       <div className="justify-self-end">
         <p>{category.name}</p>
       </div>
-      <div className="absolute -top-2 -right-2 bg-white rounded-full size-8  align-bottom p-1 shadow-md text-gray-500  text-center text-md">
+      <div className="absolute -top-2 -right-2 bg-white rounded-full size-6 p-1 align-bottom  shadow-md text-gray-500  text-center text-sm">
         {category.locations}
       </div>
     </Link>
@@ -41,7 +41,7 @@ export default async function Page() {
         </div>
         <div className="z-10 w-full mt-6">
           <div
-            className="bg-secondary rounded-full shadow-lg w-40 mx-auto max-w-xl flex flex-row py-2 px-3 items-center 
+            className="bg-secondary rounded-full shadow-lg w-40 mx-auto max-w-xl flex flex-row py-1 px-3 items-center 
       transition-all ease-out duration-300
       focus-within:bg-white focus-within:text-gray-400 focus-within:w-80 "
           >
@@ -50,7 +50,7 @@ export default async function Page() {
             <input
               type="search"
               placeholder="Пошук"
-              className="w-full bg-inherit px-4 py-2 text-lg text-white placeholder:text-gray-100 
+              className="w-full bg-inherit px-4 py-2  text-white placeholder:text-gray-100 
               
               focus:placeholder:text-gray-400 focus:outline-none focus-within:text-gray-400"
             />
